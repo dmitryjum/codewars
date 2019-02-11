91f8536311e21b7c00d8ff0444100fc5e5ae8c14
Dmitry Jum <xfiler@ymail.com>
2019-02-11 15:56:34 -0500

    coming closer

--- javascript/calendar_week/solution.js
+++ javascript/calendar_week/solution.js
@@ -7,29 +7,40 @@ function getCalendarWeek(date) {
   const firstDay = new Date(`${targetDate.getUTCFullYear()}-01-01T00:00`);
   const lastDay = new Date(`${targetDate.getUTCFullYear()}-12-31T00:00`);
   let weeksSince = (targetDate.getTime() - firstDay.getTime())/1000/60/60/168
-
+  console.log(weeksSince)
   if ((firstDay.getDay() > 4 || firstDay.getDay() === 0) && targetDate.getDate() - firstDay.getDate() < 4) {
-    if (firstDay.getDay() === 0 || firstDay.getDay() === 6) {
+    if (firstDay.getDay() === 6 && targetDate.getDate() < 3) {
       weeksSince = leapYear(firstDay.getYear - 1) ? 53 : 52
+    } else if (firstDay.getDay() === 5 && targetDate.getDate() < 4) {
+      weeksSince = 53
+    } else if (firstDay.getDay() === 0 && targetDate.getDate() < 2) {
+      weeksSince = 52
+    } else {
+      console.log(weeksSince < 1)
+      weeksSince < 1 ? weeksSince++ : weeksSince
     }
-  } else {//if (!(firstDay.getDay() > 4 || firstDay.getDay() === 0)) {
+  } else {
     weeksSince++
   }
 
   if (targetDate.getMonth() === 11) {
-    if (targetDate.getDate() > 29 && (lastDay.getDay() < 3 && lastDay.getDay > 0)) {
+    if (targetDate.getDate() > 29 && lastDay.getDay() < 3) {
       weeksSince = 1
     } else if (targetDate.getDate() > 27 && lastDay.getDay() === 4) {
       weeksSince = 53
-    } else if (targetDate.getDate() > 27 && targetDate.getDay() === 3) {
+    } else if (targetDate.getDate() > 28 && lastDay.getDay() === 3) {
       weeksSince = leapYear(targetDate.getYear()) ? 53 : 1
-    } else if (targetDate.getDate() > 24 && (targetDate.getDay() > 4 || targetDate.getDay() === 0)) {
+    } else if (targetDate.getDate() > 26 && lastDay.getDay() > 4) {
       weeksSince = 52
     }
   }
-  console.log('week', Math.floor(weeksSince))
-  return Math.floor(weeksSince);
+  weeksSince = Math.floor(weeksSince)
+  console.log(weeksSince)
+  console.log(date)
+  return weeksSince;
 }
+
+getCalendarWeek('2017-01-02'); //1
 getCalendarWeek("2016-12-26"); //52
 getCalendarWeek("2016-12-25"); //51
 getCalendarWeek("2017-01-01"); //52
