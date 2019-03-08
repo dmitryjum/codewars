1294958ca89c681b5a3eabd62bf4041c3c77205c
Dmitry Jum <xfiler@ymail.com>
2019-03-08 15:24:48 -0500

    refactors tower builder with class architecture

--- ruby/build_tower/solution.rb
+++ ruby/build_tower/solution.rb
@@ -1,19 +1,29 @@
-def stars_per_floor(floors)
-  floors*2-1
-end
-
-def towerBuilder(n_floors)
-  bottom_layer = stars_per_floor(n_floors)
-  tower = []
-  while n_floors >= 1 do
-    stars_count = stars_per_floor(n_floors)
-    space_count = bottom_layer - stars_count
-    if space_count == 0
-      tower << ('*' * (stars_per_floor(n_floors)))
-    else
-      tower << ("#{' ' * (space_count/2)}#{'*' * (stars_per_floor(n_floors))}#{' ' * (space_count/2)}")
-    end
-    n_floors = n_floors - 1
+class TowerBuilder
+  attr_accessor :floor_count
+  attr_accessor :tower
+  def initialize(n_floors)
+    @floor_count = n_floors
+    @tower = []
   end
-  tower.reverse
+
+  def build_tower
+    bottom_layer = stars_per_floor(self.floor_count)
+    while floor_count >= 1 do
+      stars_count = stars_per_floor(self.floor_count)
+      space_count = bottom_layer - stars_count
+      if space_count == 0
+        self.tower << ('*' * (stars_per_floor(self.floor_count)))
+      else
+        self.tower << ("#{' ' * (space_count/2)}#{'*' * (stars_per_floor(self.floor_count))}#{' ' * (space_count/2)}")
+      end
+      self.floor_count = self.floor_count - 1
+    end
+    self.tower = self.tower.reverse
+  end
+
+  private
+
+  def stars_per_floor(floors)
+    floors*2-1
+  end
 end
