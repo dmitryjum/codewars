647a783ad6302422fd4b424ca2a70b6f743c0f47
Dmitry Jum <xfiler@ymail.com>
2025-06-04 20:15:40 -0700

    adds section cc mask solution

--- ruby/credit_card_mask/solution2.rb
+++ ruby/credit_card_mask/solution2.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 def solution(cc)
-  "#{'#' * (cc.length - 4)}#{cc[cc.length - 4, cc.length - 1]}"
+  cc.length > 4 ? "#{'#' * (cc.length - 4)}#{cc[cc.length - 4, cc.length - 1]}" : cc
 end
 
 puts solution("4556364607935616") #"############5616"
 puts solution("64607935616")
+puts solution("111") #111
